dadaa3ae7a7ee30844bd4aefd5cf3b75a20164d9
Elzar <psixyt14@gmail.com>
2025-01-27 16:58:03 +0600

    сделал примерную структуру сайта для получения шуток

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [mem, setMem] = useState('')
+
+  const ff = async () => {
+    const apiss = await fetch('https://api.chucknorris.io/jokes/random')
+    const jsons = await apiss.json();
+    // setMem(jsons.)
+    setMem(jsons.value)
+    console.log(jsons.value)
+  }
+  // ff()
+
   return (
     <>
+      <button onClick={() => ff()}>запросить шутку</button>
+      <p>{mem}</p>
     </>
   )
 }
