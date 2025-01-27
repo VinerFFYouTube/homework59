785523d71098896ea8440605c7583bc8b9cefeeb
Elzar <psixyt14@gmail.com>
2025-01-27 17:12:34 +0600

    сделал возможность запросить несколько шуток

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,39 @@ import './App.css'
 
 function App() {
 
-  const [mem, setMem] = useState('')
+  const [mem, setMem] = useState<string[]>([])
 
-  const ff = async () => {
-    const apiss = await fetch('https://api.chucknorris.io/jokes/random')
-    const jsons = await apiss.json();
-    // setMem(jsons.)
-    setMem(jsons.value)
-    console.log(jsons.value)
-  }
+  // const ff = async () => {
+  // const apiss = await fetch('https://api.chucknorris.io/jokes/random')
+  //   const jsons = await apiss.json();
+  //   // setMem(jsons.)
+  //   setMem(jsons.value)
+  //   console.log(jsons.value)
+  // }
   // ff()
+
+  const apis = 'https://api.chucknorris.io/jokes/random'
+  const zapros = async () => {
+    const getMem = await Promise.all([
+      fetch(apis),
+      fetch(apis),
+      fetch(apis),
+      fetch(apis),
+      fetch(apis)
+    ])
+
+    const result = await Promise.all(
+      getMem.map((res) => res.json())
+    )
+    setMem(prevState => [...prevState, ...result.map(results => results.value)])
+  }
 
   return (
     <>
-      <button onClick={() => ff()}>запросить шутку</button>
-      <p>{mem}</p>
+      <button onClick={() => zapros()}>запросить шутку</button>
+      {mem.map((mems, index) => (
+        <p key={index}>{mems}</p>
+      ))}
     </>
   )
 }
